5f237773317b9a4018661dfe3c2cbb95ab36a899
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-06-27 17:37:43 +0700

    Render Props - P2

--- src/components/RenderProps/RenderProps.js
+++ src/components/RenderProps/RenderProps.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import List from './List';
+import Counter from './Counter';
 
 const data = ['A', 'B', 'C']
 function RenderProps(props) {
     return (
         <div className="RenderProps">
-            <List data={data} render={item => <div>{item}</div>} />
+            <List data={data} render={(item, idx) => <div key={idx}>{item}</div>} />
+            <List data={data} render={(item, idx) => <div key={idx}>+ {item}</div>} />
+            <List data={data} render={(item, idx) => <div key={idx}>- {item}</div>} />
+            {/* Provider */}
+            <Counter>
+                {
+                    ({ count }) =>
+                        // Consumer 
+                        <h1>{count}</h1>
+                }
+            </Counter>
         </div>
     );
 }
